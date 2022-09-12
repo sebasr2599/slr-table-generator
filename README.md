767731312fb88a37b9b77eff19597d3ad4a62069
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-09-11 19:48:12 -0500

    Proyecto finalmente terminado

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 ## Descripción
 Projecto para la clase de diseño de compiladores
-
+## Instalación
+Clonar el repositorio
+Instalar el compilador de Rust usando el siguiente comando
+```shell
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+## Compilación
+Para compilar el programa usa los sig comandos
+```shell
+cargo build --release
+cargo run --release
+```
 ## Autor
 Sebastian Resendiz
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 fn main() {
     let mut line_nums = String::new();
     let num: u32;
-    let mut final_term: HashSet<String> = HashSet::new();
-    let mut final_not_term: HashSet<String> = HashSet::new();
-    let terminals = def_terminal_map();
+    let mut final_term: BTreeSet<String> = BTreeSet::new();
+    let mut final_not_term: BTreeSet<String> = BTreeSet::new();
+    let terminals = def_terminal_set();
 
     match std::io::stdin().read_line(&mut line_nums) {
         Ok(_) => {
@@ -31,7 +30,7 @@ fn main() {
                 "expr" | "smt" => {
                     let _ = final_not_term.insert(s.to_string());
                 }
-                t if terminals.contains_key(&t.to_string()) => {
+                t if terminals.contains(&t.to_string()) => {
                     final_term.insert(s.to_string());
                 }
                 t if t.chars().all(|x| x.is_lowercase()) => {
@@ -50,50 +49,62 @@ fn main() {
 
     print_res(&final_not_term, &final_term);
 }
-fn print_res(final_not_term: &HashSet<String>, final_term: &HashSet<String>) {
-    print!("No Terminales: ");
-    for i in final_not_term {
-        print!("{i} ");
-    }
-    println!();
+fn print_res(final_not_term: &BTreeSet<String>, final_term: &BTreeSet<String>) {
     print!("Terminales: ");
-    for i in final_term {
-        print!("{i} ");
+    for (i, item) in final_term.iter().enumerate() {
+        if i != 0 {
+            print!(", ");
+        }
+        print!("{item}");
+    }
+    print!("\nNo Terminales: ");
+    for (i, item) in final_not_term.iter().enumerate() {
+        if i != 0 {
+            print!(", ");
+        }
+        print!("{item}");
     }
     println!();
 }
-fn def_terminal_map() -> HashMap<String, bool> {
+
+fn def_terminal_set() -> BTreeSet<String> {
     // 0 not terminal
     // 1 Terminal
-    HashMap::from([
-        ("+".to_string(), true),
-        ("-".to_string(), true),
-        ("*".to_string(), true),
-        ("/".to_string(), true),
-        ("%".to_string(), true),
-        ("a".to_string(), true),
-        ("b".to_string(), true),
-        ("c".to_string(), true),
-        (".".to_string(), true),
-        (",".to_string(), true),
-        (";".to_string(), true),
-        ("(".to_string(), true),
-        (")".to_string(), true),
-        ("{".to_string(), true),
-        ("}".to_string(), true),
-        ("0".to_string(), true),
-        ("1".to_string(), true),
-        ("2".to_string(), true),
-        ("3".to_string(), true),
-        ("4".to_string(), true),
-        ("5".to_string(), true),
-        ("6".to_string(), true),
-        ("7".to_string(), true),
-        ("8".to_string(), true),
-        ("9".to_string(), true),
-        ("id".to_string(), true),
-        ("if".to_string(), true),
-        ("true".to_string(), true),
-        ("false".to_string(), true),
-    ])
+    let mut set = BTreeSet::new();
+
+    set.insert("+".to_string());
+    set.insert("-".to_string());
+    set.insert("*".to_string());
+    set.insert("/".to_string());
+    set.insert("%".to_string());
+    set.insert("a".to_string());
+    set.insert("b".to_string());
+    set.insert("c".to_string());
+    set.insert("d".to_string());
+    set.insert(".".to_string());
+    set.insert(",".to_string());
+    set.insert(";".to_string());
+    set.insert("(".to_string());
+    set.insert(")".to_string());
+    set.insert("{".to_string());
+    set.insert("}".to_string());
+    set.insert("0".to_string());
+    set.insert("1".to_string());
+    set.insert("2".to_string());
+    set.insert("3".to_string());
+    set.insert("4".to_string());
+    set.insert("5".to_string());
+    set.insert("6".to_string());
+    set.insert("7".to_string());
+    set.insert("8".to_string());
+    set.insert("9".to_string());
+    set.insert("id".to_string());
+    set.insert("if".to_string());
+    set.insert("true".to_string());
+    set.insert("false".to_string());
+    set.insert("and".to_string());
+    set.insert("or".to_string());
+    set.insert("not".to_string());
+
+    set
 }
