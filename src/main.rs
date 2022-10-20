ebdbdd824d298059b442cec96d82b9afc9e7de48
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-10-20 17:10:24 -0500

    Division into productions hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashMap};
 fn main() {
-    let mut line_nums = String::new();
-    let num: u32;
+    let (final_term, final_not_term, productions) = get_terminal_and_not_terminal();
+    print_terminal_and_not_terminals(&final_not_term, &final_term);
+
+    for (key, value) in &productions {
+        println!("{} produces -> {:?}", key, value);
+    }
+}
+fn print_terminal_and_not_terminals(
+    final_not_term: &BTreeSet<String>,
+    final_term: &BTreeSet<String>,
+) {
+    print!("Terminales: ");
+    for (i, item) in final_term.iter().enumerate() {
+        if i != 0 {
+            print!(", ");
+        }
+        print!("{item}");
+    }
+    print!("\nNo Terminales: ");
+    for (i, item) in final_not_term.iter().enumerate() {
+        if i != 0 {
+            print!(", ");
+        }
+        print!("{item}");
+    }
+    println!();
+}
+fn get_terminal_and_not_terminal() -> (
+    BTreeSet<String>,
+    BTreeSet<String>,
+    HashMap<String, Vec<String>>,
+) {
     let mut final_term: BTreeSet<String> = BTreeSet::new();
     let mut final_not_term: BTreeSet<String> = BTreeSet::new();
-    let terminals = def_terminal_set();
+    let mut productions: HashMap<String, Vec<String>> = HashMap::new();
 
+    let mut line_nums = String::new();
+    let num: u32;
+    let terminals = def_terminal_set();
     match std::io::stdin().read_line(&mut line_nums) {
         Ok(_) => {
             num = line_nums.trim().parse::<u32>().expect("Not valid input");
@@ -22,6 +55,28 @@ fn main() {
             .expect("failed to read from stdin");
         let temp: Vec<&str> = temp_str.trim().split(&[' ', '\n']).collect();
         // println!("{:?}", temp);
+        // println!("{:?}", temp_str);
+
+        let left = temp_str.split("->").collect::<Vec<&str>>()[0]
+            .trim()
+            .to_string();
+        // println!("term: {:?}", left);
+
+        let right = temp_str
+            .split("->")
+            .collect::<Vec<&str>>()
+            .pop()
+            .unwrap()
+            .trim()
+            .to_string();
+        // println!("production: {:?}", right);
+
+        productions
+            .entry(left)
+            .and_modify(|vs| vs.push(right.clone()))
+            .or_insert(vec![right.clone()]);
+
+        //old way of dividing into term and !term
         for s in temp {
             // println!("{s}");
             match s {
@@ -46,27 +101,8 @@ fn main() {
             }
         }
     }
-
-    print_res(&final_not_term, &final_term);
+    return (final_term, final_not_term, productions);
 }
-fn print_res(final_not_term: &BTreeSet<String>, final_term: &BTreeSet<String>) {
-    print!("Terminales: ");
-    for (i, item) in final_term.iter().enumerate() {
-        if i != 0 {
-            print!(", ");
-        }
-        print!("{item}");
-    }
-    print!("\nNo Terminales: ");
-    for (i, item) in final_not_term.iter().enumerate() {
-        if i != 0 {
-            print!(", ");
-        }
-        print!("{item}");
-    }
-    println!();
-}
-
 fn def_terminal_set() -> BTreeSet<String> {
     // 0 not terminal
     // 1 Terminal
