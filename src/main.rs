61d8e949e2fac5d8160e037e0f38aa687ec7afa9
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-10-23 02:09:00 -0500

    Logic Done, still have to debug and test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use regex::Regex;
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use std::collections::HashSet;
+use indexmap::{IndexMap, IndexSet};
 static EPSILON: &str = "#";
+//to fix, change from hashmap, hashset to linked structures
 fn main() {
     let (final_term, final_not_term, productions) = get_terminal_and_not_terminal();
-    let mut first_and_follows: HashMap<String, (HashSet<String>, HashSet<String>)> = HashMap::new();
-    // print_terminal_and_not_terminals(&final_not_term, &final_term);
+    let mut first_and_follows: IndexMap<String, (HashSet<String>, HashSet<String>)> = IndexMap::new();
+    print_terminal_and_not_terminals(&final_not_term, &final_term);
 
     for (key, value) in &productions {
         // println!("{} produces -> {:?}", key, value);
@@ -22,65 +24,104 @@ fn main() {
         .and_modify(|tup|{
             _ = tup.1.insert("$".to_string())
         });
-    let inverted_production_map: BTreeMap<String, String> = inverted_production_map(&productions);
 
-    for b in &final_not_term {
-        _ = follow(
-            &b,
-            &inverted_production_map,
-            &mut first_and_follows,
-            &final_not_term,
-        );
+    println!("\nPrinting map of productions");
+    let inverted_production_map: IndexMap<String, String> = inverted_production_map(&productions);
+    for (key, value) in &inverted_production_map {
+        println!("Production {} produced by <- {}", key, value);
+        
     }
+
+    // println!("\nFollows process");
+    // for b in &final_not_term {
+    //     _ = follow(
+    //         &b,
+    //         &inverted_production_map,
+    //         &mut first_and_follows,
+    //         &final_not_term,
+    //     );
+    // }
     print_first_and_follows(&first_and_follows);
 }
 
 fn follow(
     b: &String,
-    map: &BTreeMap<String, String>,
-    first_and_follows: &mut HashMap<String, (HashSet<String>, HashSet<String>)>,
-    final_not_term: &BTreeSet<String>,
+    map: &IndexMap<String, String>,
+    first_and_follows: &mut IndexMap<String, (HashSet<String>, HashSet<String>)>,
+    final_not_term: &IndexSet<String>,
 ) -> HashSet<String> {
-    let formatted = format!(r"(.*) {} (.*)", b.to_string());
+    let formatted = format!(r"(.*) \b{}\b (.*)", b.to_string()); // A = αBβ
     let re_1 = Regex::new(formatted.as_str()).unwrap();
-    let formatted = format!(r"(.*) {}", b.to_string());
+    let formatted = format!(r"\b{}\b (.*)", b.to_string()); // A = Bβ
+    let re_1_2 = Regex::new(formatted.as_str()).unwrap();
+    let formatted = format!(r"(.*) \b{}\b", b.to_string()); // A = αB
     let re_2 = Regex::new(formatted.as_str()).unwrap();
 
-    for (prod, from) in map {
-        println!("Checking for B {b} in prod: {prod} gets created by: {from}");
-        if re_1.is_match(prod) {
+    for (prod, a) in map {
+        println!("Checking for th B: {b} in prod: {prod} gets created by: {a}");
+        if re_1.is_match(prod) || re_1_2.is_match(prod) {
             println!("{prod} matches rule 1");
             //make into array
-            let beta = prod.split(' ').collect::<Vec<&str>>()[2].to_string();
+            let arr = prod.split(' ').collect::<Vec<&str>>();
+            let pos =  arr.iter()
+                          .position(|&x| x == b)
+                          .unwrap()+1;
+            let beta = arr[pos].to_string();
             //check if beta its not terminal
-            let mut check = false;
             if final_not_term.contains(&beta) {
-               check = true; 
-            }
-            if check {
                 println!("{beta} is not terminal");
-                //get the frist of beta
-                // let beta_tup = first_and_follows.get_mut(&beta).unwrap();
-                // println!("first and follows of beta {:?}", beta_tup);
                 //check if the first of beta contains EPSILON
                 if first_and_follows.get(&beta).unwrap().0.contains(EPSILON) {
                     println!("beta contains EPSILON");
                     //add the first of beta except EPSILON in the follow of b
+                    //get the frist of beta
+                    let aux = first_and_follows.get(&beta).unwrap().0.clone(); //returns a set of the first of beta
+                                                                               //just add the first of beta is the follow of b
+                    first_and_follows.entry(b.to_string()).and_modify(|tup| {
+                        tup.1.extend(aux.clone());
+                    });
                     // add the follow of the producer A to follow of b
+
+                    // Check if follow of A has been calculted before?
+                    if !first_and_follows.get(a).unwrap().1.is_empty() {
+                        // if so, just append it
+                        println!("The follow of A {a} is not empty!");
+                        let aux = first_and_follows.get(a).unwrap().0.clone(); //returns a set of the first of beta
+                        println!("appending the the first of A: to the follow of B {:?}", aux);
+                        first_and_follows.entry(b.to_string()).and_modify(|tup| {
+                            tup.1.extend(aux);
+                        });
+                    } else {
+                        // else append the calculation of the follow of A to the follow of B
+                        println!("The follow of A {a}  empty!");
+                        let aux = follow(a, map, first_and_follows, final_not_term);
+                        first_and_follows.entry(b.to_string()).and_modify(|tup| {
+                            tup.1.extend(aux);
+                        });
+                    }
                 } else {
                     println!("beta does not contains EPSILON");
-                    //just add the first of beta to follow of b
-                    let aux = first_and_follows.get(&beta).unwrap().0.clone();
-                    first_and_follows
-                        .entry(b.to_string())
-                        .and_modify(|tup|{
-                            tup.1 = aux
-                        });
+                    //get the frist of beta
+                    let aux = first_and_follows.get(&beta).unwrap().0.clone(); //returns a set of the first of beta
+                                                                               //just add the first of beta is the follow of b
+                    first_and_follows.entry(b.to_string()).and_modify(|tup| {
+                        tup.1.extend(aux);
+                    });
                 }
+            } else {
+                first_and_follows
+                    .entry(b.to_string())
+                    .and_modify(|tup|
+                                { _ = tup.1.insert(beta.to_string())
+                                });
             }
         } else if re_2.is_match(prod) {
             println!("{prod} matches rule 2");
-            // add the follow of the producer A to follow of b
+            // else append the calculation of the follow of A to the follow of B
+            let aux = follow(a, map, first_and_follows, final_not_term);
+            first_and_follows.entry(b.to_string()).and_modify(|tup| {
+                tup.1.extend(aux);
+            });
         } else {
             //remove
             println!("{prod} matches no rule");
@@ -89,13 +130,13 @@ fn follow(
     first_and_follows.get(b).unwrap().1.clone()
 }
 fn inverted_production_map(
-    productions_map: &HashMap<String, Vec<String>>,
-) -> BTreeMap<String, String> {
-    let mut out: BTreeMap<String, String> = BTreeMap::new();
+    productions_map: &IndexMap<String, Vec<String>>,
+) -> IndexMap<String, String> {
+    let mut out: IndexMap<String, String> = IndexMap::new();
     for (key, value) in productions_map {
         for str_vec in value {
             out.entry(str_vec.to_string()).or_insert(key.to_string());
-            println!("{str_vec} -> {key}");
+            // println!("{str_vec} -> {key}");
         }
     }
     out
@@ -103,18 +144,16 @@ fn inverted_production_map(
 fn first(
     not_term: &String,
     productions: &Vec<String>,
-    terminals: &BTreeSet<String>,
-    output_map: &mut HashMap<String, (HashSet<String>, HashSet<String>)>,
-    productions_map: &HashMap<String, Vec<String>>,
+    terminals: &IndexSet<String>,
+    output_map: &mut IndexMap<String, (HashSet<String>, HashSet<String>)>,
+    productions_map: &IndexMap<String, Vec<String>>,
 ) -> HashSet<String> {
     for production in productions {
         let front = production.split(' ').collect::<Vec<&str>>()[0].to_string();
         if terminals.contains(&front) {
             output_map
                 .entry(not_term.clone())
-                .and_modify(|(frst, _)| {
-                    _ = frst.insert(front.clone())
-                })
+                .and_modify(|(frst, _)|{ _ = frst.insert(front.clone())})
                 .or_insert((HashSet::from([front.clone()]), HashSet::new()));
         } else {
             let t = first(
@@ -132,17 +171,17 @@ fn first(
     output_map.get(not_term).unwrap().0.clone()
 }
 fn get_terminal_and_not_terminal() -> (
-    BTreeSet<String>,
-    BTreeSet<String>,
-    HashMap<String, Vec<String>>,
+    IndexSet<String>,
+    IndexSet<String>,
+    IndexMap<String, Vec<String>>,
 ) {
-    let mut final_term: BTreeSet<String> = BTreeSet::new();
-    let mut final_not_term: BTreeSet<String> = BTreeSet::new();
-    let mut productions: HashMap<String, Vec<String>> = HashMap::new();
+    let mut final_term: IndexSet<String> = IndexSet::new();
+    let mut final_not_term: IndexSet<String> = IndexSet::new();
+    let mut productions: IndexMap<String, Vec<String>> = IndexMap::new();
 
     let mut line_nums = String::new();
     let num: u32;
-    let terminals = def_terminal_set();
+    // let terminals = def_terminal_set();
     match std::io::stdin().read_line(&mut line_nums) {
         Ok(_) => {
             num = line_nums.trim().parse::<u32>().expect("Not valid input");
@@ -157,8 +196,8 @@ fn get_terminal_and_not_terminal() -> (
         std::io::stdin()
             .read_line(&mut temp_str)
             .expect("failed to read from stdin");
-        temp_str = temp_str.replace("\' \'", "#");
-        let temp: Vec<&str> = temp_str.trim().split(&[' ', '\n']).collect();
+        temp_str = temp_str.replace("\' \'", EPSILON);
+        // let temp: Vec<&str> = temp_str.trim().split(&[' ', '\n']).collect();
         // println!("{:?}", temp);
         // println!("{:?}", temp_str);
 
@@ -177,41 +216,53 @@ fn get_terminal_and_not_terminal() -> (
         // println!("production: {:?}", right);
 
         productions
-            .entry(left)
+            .entry(left.to_string())
             .and_modify(|vs| vs.push(right.clone()))
             .or_insert(vec![right.clone()]);
 
-        //old way of dividing into term and !term
-        for s in temp {
-            // println!("{s}");
-            match s {
-                "->" => (),
-                // "expr" => final_not_term.insert(s.to_string()),
-                "expr" | "smt" => {
-                    let _ = final_not_term.insert(s.to_string());
-                }
-                t if terminals.contains(&t.to_string()) => {
-                    final_term.insert(s.to_string());
-                }
-                t if t.chars().all(|x| x.is_lowercase()) => {
-                    final_not_term.insert(s.to_string());
-                }
-                t if t.chars().all(|x| x.is_uppercase()) => {
-                    final_not_term.insert(s.to_string());
-                }
-                t if t == '\''.to_string() => (),
-                _ => {
-                    final_not_term.insert(s.to_string());
-                }
-            }
+        final_not_term.insert(left.to_string());
+        let aux:Vec<&str> = right.split(' ').collect();
+        for x in aux {
+           final_term.insert(x.to_string());
         }
+        //old way of dividing into term and !term
+        
+        // for s in temp {
+        //     // println!("{s}");
+        //     match s {
+        //         "->" => (),
+        //         // "expr" => final_not_term.insert(s.to_string()),
+        //         "expr" | "smt" => {
+        //             let _ = final_not_term.insert(s.to_string());
+        //         }
+        //         t if terminals.contains(&t.to_string()) => {
+        //             final_term.insert(s.to_string());
+        //         }
+        //         t if t.chars().all(|x| x.is_lowercase()) => {
+        //             final_not_term.insert(s.to_string());
+        //         }
+        //         t if t.chars().all(|x| x.is_uppercase()) => {
+        //             final_not_term.insert(s.to_string());
+        //         }
+        //         t if t == '\''.to_string() => (),
+        //         _ => {
+        //             final_not_term.insert(s.to_string());
+        //         }
+        //     }
+        // }
+    }
+    for x in &final_not_term {
+       if final_term.contains(x) {
+           let i = x.clone();
+           _ = final_term.shift_remove(&i);
+       } 
     }
     return (final_term, final_not_term, productions);
 }
-fn def_terminal_set() -> BTreeSet<String> {
+fn _def_terminal_set() -> IndexSet<String> {
     // 0 not terminal
     // 1 Terminal
-    let mut set = BTreeSet::new();
+    let mut set = IndexSet::new();
 
     set.insert("+".to_string());
     set.insert("#".to_string());
@@ -250,9 +301,9 @@ fn def_terminal_set() -> BTreeSet<String> {
 
     set
 }
-fn _print_terminal_and_not_terminals(
-    final_not_term: &BTreeSet<String>,
-    final_term: &BTreeSet<String>,
+fn print_terminal_and_not_terminals(
+    final_not_term: &IndexSet<String>,
+    final_term: &IndexSet<String>,
 ) {
     print!("Terminales: ");
     for (i, item) in final_term.iter().enumerate() {
@@ -271,7 +322,7 @@ fn _print_terminal_and_not_terminals(
     println!();
 }
 fn print_first_and_follows(
-    first_and_follows: &HashMap<String, (HashSet<String>, HashSet<String>)>,
+    first_and_follows: &IndexMap<String, (HashSet<String>, HashSet<String>)>,
 ) {
     for (key, value) in first_and_follows {
         print!("{} FIRST = {{", key);
